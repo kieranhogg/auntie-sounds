6ccc88f5886f321bb0e29b3b7f3f14393e8f9b85
Kieran Hogg <kieran.hogg@gmail.com>
2025-07-09 08:43:23 +0100

    Get local stations; use get_stations() in get_station()

--- src/sounds/stations.py
+++ src/sounds/stations.py
@@ -1,3 +1,4 @@
+import itertools
 from . import constants
 from .base import Base
 from .constants import URLs
@@ -7,7 +8,7 @@ from .utils import network_logo
 
 class StationsService(Base):
 
-    async def get_stations(self) -> list[Station]:
+    async def get_stations(self, include_local=False) -> list[Station]:
         """
         Gets the list of all stations
 
@@ -17,17 +18,37 @@ class StationsService(Base):
         json_resp = await self._get_json(URLs.STATIONS_URL)
         self.logger.log(constants.VERBOSE_LOG_LEVEL, "Getting station list...")
         self.logger.log(constants.VERBOSE_LOG_LEVEL, json_resp)
+
+        # Append a key to assign if they are local stations or not
+        for station in json_resp["data"][0]["data"]:
+            station["local"] = False
+
+        for station in json_resp["data"][1]["data"]:
+            station["local"] = True
+
+        if include_local:
+            # Flatten the national and local stations sublists
+            stations = list(
+                itertools.chain(
+                    json_resp["data"][0]["data"], json_resp["data"][1]["data"]
+                )
+            )
+
+        else:
+            # Just get the national data list
+            stations = json_resp["data"][0]["data"]
         return [
             Station(
                 id=s["id"],
                 name=s["network"]["short_title"],
                 description=f"{s['titles']['secondary']} • {s['titles']['primary']}: {s['synopses']['short']}",
                 logo_url=network_logo(s["network"]["logo_url"]),
+                local=s["local"],
             )
-            for s in json_resp["data"][0]["data"]
+            for s in stations
         ]
 
-    async def get_station(self, station_id: str) -> Station:
+    async def get_station(self, station_id: str) -> Station | None:
         """
         Gets a station's details
 
@@ -35,15 +56,8 @@ class StationsService(Base):
         :rtype: Station
         """
         # TODO: include listings here?
-        json_resp = await self._get_json(URLs.STATIONS_URL)
-        self.logger.log(constants.VERBOSE_LOG_LEVEL, "Getting station list...")
-        self.logger.log(constants.VERBOSE_LOG_LEVEL, json_resp["data"][0]["data"])
-        station_data = [
-            station for station in json_resp if station["id"] == station_id
-        ][0]
-        return Station(
-            id=station_data["id"],
-            name=station_data["network"]["short_title"],
-            description=f"{station_data['titles']['secondary']} • {station_data['titles']['primary']}: {station_data['synopses']['short']}",
-            logo_url=network_logo(station_data["network"]["logo_url"]),
-        )
+        stations = await self.get_stations(include_local=True)
+        try:
+            return [station for station in stations if station.id == station_id][0]
+        except KeyError:
+            return None
