f44c566d652926140b0834c6d7943696b395fe31
Kieran Hogg <kieran.hogg@gmail.com>
2025-07-09 08:42:38 +0100

    Update docstring

--- src/sounds/models.py
+++ src/sounds/models.py
@@ -5,7 +5,23 @@ from pprint import pformat
 
 @dataclass
 class Stream:
-    """A container for holding attributes related to a stream"""
+    """
+    Represents a station stream.
+
+    Attributes:
+        id (str): Unique identifier for the stream.
+        start (datetime): Start time of the stream.
+        end (datetime): End time of the stream.
+        uri (str): URL to access the stream.
+        image_url (str): URL to a thumbnail for the current programme.
+        network (str): Name of the station.
+        network_logo (str): URL to the station's logo image.
+        show_title (str): Title of the show being streamed.
+        show_description (str): Description of the show.
+
+    Properties:
+        can_seek (bool): Indicates if the stream supports seeking. (Always False for now.)
+    """
 
     id: str
     start: dt
@@ -25,12 +41,22 @@ class Stream:
 
 @dataclass
 class Station:
-    """A container for holding attributes related to a station"""
+    """
+    Represents a radio or media station with its metadata.
+
+    Attributes:
+        id (str): Unique identifier for the station.
+        name (str): Human-readable name of the station.
+        description (str): Description of the station's content or focus.
+        logo_url (str): URL to the station's logo image.
+        local (bool): Flag indicating whether the station is considered local.
+    """
 
     id: str
     name: str
     description: str
     logo_url: str
+    local: bool
 
     def __str__(self):
         return pformat(self)
@@ -38,7 +64,21 @@ class Station:
 
 @dataclass
 class Segment:
-    """A container for holding attributes related to a segment item (usually a song)"""
+    """
+    Represents a segment within a stream, such as a song or program section.
+
+    Attributes:
+        id (str): Unique identifier for the segment.
+        primary_title (str): Primary title (e.g., song title or segment name).
+        secondary_title (str): Secondary title (e.g., artist or contributor).
+        tertiary_title (str): Appears to be unused for most segments.
+        entity_title (str): Appears to always be the same as primary_title.
+        image_url (str): URL to an image representing the segment.
+        start_seconds (int): Start time of the segment in seconds.
+        end_seconds (int): End time of the segment in seconds.
+        label (str): Text description of either 'Now playing' or e.g. '2 minutes ago'
+        now_playing (bool): Indicates whether this segment is currently playing.
+    """
 
     id: str
     primary_title: str
