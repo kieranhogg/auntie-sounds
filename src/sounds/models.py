1d2e3aa4caf0b3192304d4aa8fb48fa11ccd29d0
Kieran Hogg <kieran.hogg@gmail.com>
2025-07-23 20:24:58 +0100

    No pydantic for now

--- src/sounds/models.py
+++ src/sounds/models.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from datetime import datetime as dt
-from pydantic import BaseModel, ConfigDict, ValidationError
 from pprint import pformat
 from typing import List, Optional
 
