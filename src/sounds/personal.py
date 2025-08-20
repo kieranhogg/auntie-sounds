058957a089ab95d6b558e07c59af40f51f68a678
Kieran Hogg <kieran.hogg@gmail.com>
2025-08-20 11:02:44 +0100

    Allow recommendation filter in menu

--- src/sounds/personal.py
+++ src/sounds/personal.py
@@ -1,8 +1,17 @@
-from .json import parse_menu
-from .auth import AuthService
+from enum import Enum
+
+from .models import RecommendedMenuItem
+from .json import parse_menu, parse_container
+from .auth import AuthService, login_required
 from .base import Base
-from .constants import URLs
-from .exceptions import APIResponseError
+from .constants import URLs, SignedInURLs
+from .exceptions import APIResponseError, UnauthorisedError
+
+
+class MenuRecommendationOptions(Enum):
+    EXCLUDE = "Exclude"
+    INCLUDE = "Include"
+    ONLY = "Only"
 
 
 class PersonalService(Base):
@@ -14,15 +23,28 @@ class PersonalService(Base):
         super().__init__(**kwargs)
         self.auth = auth_service
 
-    # @login_required
-    async def get_experience_menu(self):
+    @login_required
+    async def get_experience_menu(
+        self,
+        recommendations: MenuRecommendationOptions = MenuRecommendationOptions.INCLUDE,
+    ):
         """Gets the main Sounds menu."""
-        json_resp = await self._get_json(URLs.EXPERIENCE_MENU)
-
-        if not json_resp or "data" not in json_resp:
-            raise APIResponseError(
-                f"Couldn't get the main menu:\n{json_resp.get("message")}"
-            )
-
+        json_resp = await self._get_json(url_template=URLs.EXPERIENCE_MENU)
         menu = parse_menu(json_resp)
+        if recommendations == MenuRecommendationOptions.EXCLUDE:
+            filtered_menu = [
+                item for item in menu.sub_items if type(item) is not RecommendedMenuItem
+            ]
+        elif recommendations == MenuRecommendationOptions.ONLY:
+            filtered_menu = [
+                item for item in menu.sub_items if type(item) is RecommendedMenuItem
+            ]
+        else:
+            filtered_menu = menu.sub_items
+        menu.sub_items = filtered_menu
         return menu
+
+    @login_required
+    async def get_subscriptions(self):
+        json_resp = await self._get_json(url_template=SignedInURLs.SUBSCRIBED)
+        return parse_container(json_resp)
