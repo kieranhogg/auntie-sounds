aec188c96e3bc4790f81ae5705e0261c005fa4f5
Kieran Hogg <kieran.hogg@gmail.com>
2025-08-20 11:03:07 +0100

    Use parser for get_schedule

--- src/sounds/schedules.py
+++ src/sounds/schedules.py
@@ -4,60 +4,50 @@ from . import constants
 from .base import Base
 from .constants import URLs
 from .exceptions import InvalidFormatError
-from .json import parse_menu
-from .models import ScheduleItem, Segment, Station
+from .json import parse_container, parse_menu, parse_node, parse_schedule
+from .models import Schedule, ScheduleItem, Segment, Station
 from .utils import image_from_recipe
 
 
 class ScheduleService(Base):
     async def get_schedule(
         self, station_id: str, date: str | None = None, image_size=320
-    ) -> list[ScheduleItem]:
-        url = URLs.SCHEDULE_URL.format(station_id=station_id)
+    ) -> Schedule:
+        url_template = URLs.SCHEDULE
         if date:
+            url_template = URLs.SCHEDULE_DATE
             try:
                 _ = dt.strptime(date, "%Y-%m-%d")
             except ValueError:
                 raise InvalidFormatError(
                     "Invalid date specified, must be in the format YYYY-MM-DD"
                 )
-            url = f"{url}/{date}"
-        json_resp = await self._get_json(url)
-        schedule = json_resp["data"][0]["data"]
-        print(parse_menu(json_resp))
-        exit(0)
-        self.logger.log(constants.VERBOSE_LOG_LEVEL, "Getting schedule for list...")
-        self.logger.log(constants.VERBOSE_LOG_LEVEL, json_resp)
-        return [
-            ScheduleItem(
-                id=item["id"],
-                urn=item["urn"],
-                start=dt.fromisoformat(item["start"]),
-                end=dt.fromisoformat(item["end"]),
-                duration=item["duration"],
-                short_synopsis=item["synopses"]["short"],
-                medium_synopsis=item["synopses"]["medium"],
-                long_synopsis=item["synopses"]["long"],
-                image_url=image_from_recipe(
-                    image_recipe=item["image_url"], size=image_size
-                ),
-                primary_title=item["titles"]["primary"],
-                secondary_title=item["titles"]["secondary"],
-                tertiary_title=item["titles"]["tertiary"],
-                episode_id=(
-                    item["playable_item"]["id"] if item["playable_item"] else None
-                ),
-            )
-            for item in schedule
-        ]
+        json_resp = await self._get_json(
+            url_template=url_template, url_args={"station_id": station_id, "date": date}
+        )
+        return parse_schedule(json_resp)
+
+    async def current_programme(self, station_id: str):
+        json_resp = await self._get_json(url_template=constants.URLs.STATIONS)
+        listing = next(
+            (
+                station
+                for station in json_resp["data"][0]["data"]
+                if station.get("id") == station_id
+            ),
+            None,
+        )
+        return parse_node(listing)
 
     async def recently_played_items(
-        self, station: Station, image_size=450, results=10
+        self, station_id: str, image_size=450, results=10
     ) -> list[Segment]:
         """Gets the recent playing items on this station"""
-        url = URLs.NOW_PLAYING_URL.format(station_id=station.id, limit=results)
-        json = await self._get_json(url)
-
+        json_resp = await self._get_json(
+            url_template=URLs.NOW_PLAYING,
+            url_args={"station_id": station_id, "limit": results},
+        )
+        return parse_container(json_resp)
         return [
             Segment(
                 id=segment["id"],
@@ -67,7 +57,7 @@ class ScheduleService(Base):
                 entity_title=segment["titles"]["entity_title"],
                 image_url=image_from_recipe(segment["image_url"], image_size),
                 start_seconds=segment["offset"]["start"],
-                end_seconds=segment["offset"]["start"],
+                end_seconds=segment["offset"]["end"],
                 label=segment["offset"]["label"],
                 now_playing=segment["offset"]["now_playing"],
             )
@@ -80,8 +70,8 @@ class ScheduleService(Base):
         """Gets the currently playing song, if one is playing."""
         recently_played = await self.recently_played_items(station_id, image_size)
         try:
-            if recently_played[0].now_playing:
+            if recently_played[0].offset["now_playing"]:
                 return recently_played[0]
-        except KeyError:
+        except IndexError:
             pass
         return None
