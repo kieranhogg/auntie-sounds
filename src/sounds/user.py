7bb7a58bf528fb5cc28048e1161d791372edfa12
Kieran Hogg <kieran.hogg@gmail.com>
2026-06-25 16:16:51 +0100

    Refactor user_info

--- src/sounds/user.py
+++ src/sounds/user.py
@@ -1,28 +1,34 @@
 from sounds.base import Base
 from sounds.constants import URLs
+from sounds.session import Session
 
 
 class UserService(Base):
-    def __init__(self) -> None:
-        self.user_info: dict[str, str] | None = None
+    def __init__(
+        self, state: Session, login_details_provided: bool, *args, **kwargs
+    ) -> None:
+        super().__init__(state=state, *args, **kwargs)
+        self._user_info: dict[str, str] = dict()
+        self.login_details_provided = login_details_provided
 
     async def refresh(self) -> None:
-        self.user_info = await self._get_json(url_template=URLs.USER_INFO)
+        self._user_info = await self._get_json(url_template=URLs.USER_INFO)
+
+    async def _ensure_loaded(self):
+        if not self._user_info:
+            await self.refresh()
 
     async def listener_country(self) -> str | None:
         """Return the listener's current country."""
-        if self.user_info:
-            return self.user_info.get("X-Country")
-        return None
+        await self._ensure_loaded()
+        return self._user_info.get("X-Country")
 
     async def is_in_uk(self) -> bool:
         """Listener is in the UK."""
-        if self.user_info:
-            return self.user_info.get("X-Country") == "gb"
-        return False
+        await self._ensure_loaded()
+        return self._user_info.get("X-Country") == "gb"
 
     async def is_uk_listener(self) -> bool:
         """Listener has a UK-based account and is in the UK."""
-        if self.user_info:
-            return self.user_info["X-Ip_is_uk_combined"] == "yes"
-        return False
+        await self._ensure_loaded()
+        return self._user_info["X-Ip_is_uk_combined"] == "yes"
