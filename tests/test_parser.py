725e3dc0242be40cea0930a0b38090c4794c59d9
Kieran Hogg <kieran.hogg@gmail.com>
2026-09-10 15:33:52 +0100

    test: add nested object tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,4 +1,9 @@
-from sounds.models import Menu, Podcast, PodcastEpisode, SearchResults
+import json
+from logging import Logger
+
+import pytest
+
+from sounds.models import Menu, Podcast, PodcastEpisode, RadioShow, SearchResults
 from sounds.parser import Parser
 
 
@@ -32,3 +37,56 @@ class TestParser:
         assert hasattr(result, "stations")
         assert hasattr(result, "shows")
         assert hasattr(result, "episodes")
+
+
+class TestParserNestedObjects:
+    def test_ignored_nested_objects(self, logger: Logger):
+        """Test that that embedded objects with keys in the ignore_objects list are removed."""
+        parser = Parser(logger)
+        with open("tests/fixtures/api/PROGRAMME_FROM_PID_PLAYABLE.json") as json_file:
+            json_data = json.loads(json_file.read())
+            for key in parser.ignored_objects:
+                assert key in json_data
+                result = parser.parse_node(json_data)
+                assert type(result) is RadioShow
+                with pytest.raises(AttributeError):
+                    getattr(result, key)
+
+    def test_nested_objects(self, logger: Logger):
+        """Test that that embedded objects with keys in the nested_objects list are parsed as objects too.
+
+        Currently:
+            NestedObject("network", Network),
+            NestedObject("container", Container),
+        """
+
+        parser = Parser(logger)
+
+        data = {
+            "type": "playable_item",
+            "id": "p0p78bd4",
+            "urn": "urn:bbc:radio:episode:m003169g",
+            "network": {
+                "id": "bbc_radio_four",
+                "key": "radio4",
+                "short_title": "Radio 4",
+                "logo_url": "https://sounds.files.bbci.co.uk/3.12.0/networks/bbc_radio_four/{type}_{size}.{format}",
+                "network_type": "master_brand",
+            },
+            "container": {
+                "type": "brand",
+                "id": "p0h0q056",
+                "urn": "urn:bbc:radio:brand:p0h0q056",
+                "title": "The Traitors: Uncloaked",
+                "synopses": {
+                    "short": "Ed Gamble hosts the official Traitors visualised podcast with unseen bonus content.",
+                    "medium": "Ed Gamble brings you exclusive unseen content from the castle as the banished and murdered players find out who the Traitors are.\n\nWatch on iPlayer here: https://bbc.in/48ilW3e",
+                    "long": "Ed Gamble hosts the official visualised podcast for the ultimate game of trust and treachery. \n\nAlongside celebrity guests and fans, Ed brings you exclusive unseen content.\n\nYou can watch Uncloaked on iPlayer here: https://bbc.in/48ilW3e",
+                },
+                "activities": [],
+            },
+        }
+        parser = Parser(logger)
+        result = parser.parse_node(data)
+        for nested_object in parser.nested_objects:
+            assert hasattr(result, nested_object.source_key)
