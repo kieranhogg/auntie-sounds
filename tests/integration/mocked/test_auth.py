62aaf2902ffdbffd54034b122c47112a87e3129e
Kieran Hogg <kieran.hogg@gmail.com>
2026-09-24 20:28:45 +0100

    refactor: formatting tweaks

--- tests/integration/mocked/test_auth.py
+++ tests/integration/mocked/test_auth.py
@@ -8,10 +8,12 @@ def username_form():
     with open("tests/fixtures/auth/login_form.html") as f:
         return f.read()
 
+
 @pytest.fixture
 def password_form():
     with open("tests/fixtures/auth/password_form.html") as f:
         return f.read()
+
 
 class TestAuthHelpers:
     def test_getting_form_action_from_username_form(self, username_form):
@@ -23,4 +25,3 @@ class TestAuthHelpers:
         action = _get_form_action(password_form)
         assert action.startswith("/auth/password?")
         assert "action=sign-in" in action
-
